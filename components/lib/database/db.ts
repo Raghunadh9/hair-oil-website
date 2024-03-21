f573defbac7977b597d6c256f990ee097c158f3a
Raghunadh9 <konidoniravirr@gmail.com>
2024-03-21 20:11:17 +0530

    ok

--- components/lib/database/db.ts
+++ components/lib/database/db.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "Hair Oil",
+      dbName: "hair-Oil",
       bufferCommands: false,
     });
 
