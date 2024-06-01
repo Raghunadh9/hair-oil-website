a103e905c4b81694ea5041f035d4f84bb921c2df
Raghunadh9 <konidoniravirr@gmail.com>
2024-06-01 13:48:29 +0530

    ok bro, final

--- app/api/mobile/product/[id]/route.ts
+++ app/api/mobile/product/[id]/route.ts
@@ -1,7 +1,21 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/components/lib/database/db";
 import Product from "@/components/lib/database/models/product.model";
+import Category from "@/components/lib/database/models/category.model";
+import SubCategory from "@/components/lib/database/models/subCategory.model";
+import User from "@/components/lib/database/models/user.model";
 export const dynamic = "force-dynamic";
+function calculatePercentage(num: any, product: any) {
+  return (
+    (product.reviews.reduce((a: any, review: any) => {
+      return (
+        a + (review.rating == Number(num) || review.rating == Number(num) + 0.5)
+      );
+    }, 0) *
+      100) /
+    product.reviews.length
+  ).toFixed(1);
+}
 export const POST = async (
   req: Request,
   { params, searchParams }: { params: any; searchParams: any }
@@ -11,31 +25,75 @@ export const POST = async (
     const { id } = params;
     const body = await req.json();
     const { style, size } = body;
-    const product: any = await Product.findById(id).lean();
-
-    let discount = product.subProducts[style].discount;
-    let priceBefore = product.subProducts[style].sizes[size].price;
-    let price = discount ? priceBefore - priceBefore / discount : priceBefore;
-    let data = {
-      _id: product._id.toString(),
-      style: Number(style),
-      name: product.name,
-      description: product.description,
-      slug: product.slug,
-      sku: product.subProducts[style].sku,
-      brand: product.brand,
-      category: product.category,
-      subCategories: product.subCategories,
-      shipping: product.shipping,
-      images: product.subProducts[style].images,
-      color: product.subProducts[style].color,
-      size: product.subProducts[style].sizes[size].size,
-      price,
-      priceBefore,
-      vendor: product.vendor,
-      quantity: product.subProducts[style].sizes[size].qty,
+    let product: any = await Product.findById(id)
+      .populate({ path: "category", model: Category })
+      .populate({ path: "subCategories", model: SubCategory })
+      .populate({ path: "reviews.reviewBy", model: User })
+      .lean();
+    let subProduct = product.subProducts[style];
+    let prices = subProduct.sizes
+      .map((s: any) => {
+        return s.price;
+      })
+      .sort((a: any, b: any) => {
+        return a - b;
+      });
+    let newProduct = {
+      ...product,
+      style,
+      images: subProduct.images,
+      sizes: subProduct.sizes,
+      discount: subProduct.discount,
+      sku: subProduct.sku,
+      colors: product.subProducts.map((p: any) => {
+        return p.color;
+      }),
+      priceRange:
+        prices.length > 1
+          ? `From Rs.${prices[0]} to Rs.${prices[prices.length - 1]}`
+          : "",
+      price:
+        subProduct.discount > 0
+          ? (
+              subProduct.sizes[size].price -
+              subProduct.sizes[size].price / subProduct.discount
+            ).toFixed(2)
+          : subProduct.sizes[size].price,
+      priceBefore: subProduct.sizes[size].price,
+      quantity: subProduct.sizes[size].qty,
+      ratings: [
+        {
+          percentage: calculatePercentage("5", product),
+        },
+        {
+          percentage: calculatePercentage("4", product),
+        },
+        {
+          percentage: calculatePercentage("3", product),
+        },
+        {
+          percentage: calculatePercentage("2", product),
+        },
+        {
+          percentage: calculatePercentage("1", product),
+        },
+      ],
+      allSizes: product.subProducts
+        .map((p: any) => {
+          return p.sizes;
+        })
+        .flat()
+        .sort((a: any, b: any) => {
+          return a.size - b.size;
+        })
+        .filter(
+          (element: any, index: any, array: any) =>
+            array.findIndex((el2: any) => el2.size === element.size) === index
+        ),
     };
-    return NextResponse.json(JSON.parse(JSON.stringify(data)), { status: 200 });
+    return NextResponse.json(JSON.parse(JSON.stringify(newProduct)), {
+      status: 200,
+    });
   } catch (error: any) {
     return new NextResponse(`${error.message}`, {
       status: 500,
